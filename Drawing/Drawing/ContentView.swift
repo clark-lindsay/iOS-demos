b05c0a10b1c0975620a57e40b1e11b1c1cc97f66
clark-lindsay <clarklindsay96@gmail.com>
2020-08-18 17:26:27 -0400

    playing with drawing again

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -63,16 +63,41 @@ struct Arc: InsettableShape {
     }
 }
 
-struct ContentView: View {
-    var body: some View {
-        ZStack {
-            Hexagon()
-                .strokeBorder(Color.green, style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round))
-                .frame(width: 275, height: 250)
+struct Flower: Shape {
+    var petalOffset: Double = -20
+    var petalWidth: Double = 100
+    
+    func path(in rect: CGRect) -> Path {
+        var result = Path()
+        
+        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8) {
+            let rotation = CGAffineTransform(rotationAngle: number)
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
+            let rotatedPetal = originalPetal.applying(position)
             
-            Arc(startAngle: .degrees(0), endAngle: .degrees(180), clockwise: false)
-                .strokeBorder(Color.blue, style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round))
-            .frame(width: 275, height: 250)
+            result.addPath(rotatedPetal)
+        }
+        return result
+    }
+
+}
+
+struct ContentView: View {
+    @State private var petalWidth = 100.0
+    @State private var petalOffset = -20.0
+    
+    var body: some View {
+        VStack {
+            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+                .fill(Color.green, style: FillStyle(eoFill: true))
+                .frame(width: 300, height: 300)
+            Text("Petal Width")
+            Slider(value: $petalWidth, in: 1...200)
+                .padding([.bottom, .horizontal])
+            Text("Petal Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding([.bottom, .horizontal])
         }
     }
 
